7d794bdebab075ef21414ec42dfda52e681dd309
subokim <kimsubo@gmail.com>
2024-05-08 15:01:27 +0900

    update example

--- poet/main.py
+++ poet/main.py
@@ -20,3 +20,40 @@ output_parser = StrOutputParser()
 
 chain = prompt | llm | output_parser
 chain.invoke({"input": "how can langsmith help with testing?"})
+
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+docs = loader.load()
+
+from langchain_openai import OpenAIEmbeddings
+embeddings = OpenAIEmbeddings()
+
+from langchain_community.vectorstores import faiss
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = faiss.from_documents(documents, embeddings)
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+from langchain_core.documents import Document
+document_chain.invoke({
+    "input" : "how can langsmith help with testing?",
+    "context" : [Document(page_content="langsmith can let you visualize test results")]
+})
+
+from langchain.chains import create_retrieval_chain
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input" : "how can langsmith help with testing?"})
+print(response["answer"])
